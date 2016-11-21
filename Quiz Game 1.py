3832f5a9dc6ecc700a17680d552460978c88894b
200262 <200262@hkis.edu.hk>
2016-11-21 10:32:58 +0800

    added questions
    added questions, changed one line to show correct answers

--- Quiz Game 1.py
+++ Quiz Game 1.py
@@ -13,8 +13,16 @@ questions = ["Question One: What is the seventh planet from the sun? A. Pluto B.
              "Question Five: How many symphonies did Beethoven compose? A. 12 B. 9 C. 0 D. 39",
              "Question Six: Which of the following is not a major blood group? A. A B. B C. P D. O ",
              "Question Seven: What is Earth's diameter? A. 8000 miles B. 800 miles C. 10000 kilometers D. It has not been measured yet",
+             "Question Eight: How old is Donald Trump? A. 62 B. 10 C. 70 D. 65",
+             "Question Nine: How much dirt is there in a hole 4 ft deep by 3 ft wide by 6 ft long? A. 0 cubic ft B. 360 cubic ft C. 100 cubic ft, D. 24 cubic yards",
+             "Question Ten: How many US states border the Gulf of Mexico? A. 7 B. 0 C. 1 D. 5",
+             "Question Eleven: What colour is Absynth? A. Yellow B. Green C. Orange D. Absynth is not a color",
+             "Question Twelve: How many patents did Thomas Edison average per week of his life? A. 17 B. 1/3 C. less than 1/4 D. 2",
+             "Question Thirteen: How many times can you subtract 10 from 100? A. Once B. Twice C. 10 times D. an infinite amount of times",
+             "Question Fourteen: How many plays did Shakespeare write? A. 37 B. 12 C. He didn't write them D. 56",
+             "Question Fifteen: A rooster laid an egg on top of a barn roof, which way did it roll? A. left B. right C. it broke D. none of the above",
              ]
-answers = ["D", "A", "D", "C", "B", "C", "A"]
+answers = ["D", "A", "D", "C", "B", "C", "A", "C", "A", "D", "B", "B", "A", "A", "D", ]
 totalquestions=len(questions)
 answerinput=0
 score = 0
@@ -37,10 +45,11 @@ def answer(questionnumber):
         global answerinput
         if str(answerinput) == str(answers[questionnumber]):
             score=score+1
-            print ("You're score is:", str(score))
+            print ("You were correct. You're score is:", str(score))
             questionsasked=questionsasked+1
             question(questionnumber+1)
         else:
+            print ("You were incorrect, the correct answer was:", str(answers[questionnumber]))
             print ("You're score is:", str(score))
             questionsasked=questionsasked+1
             question(questionnumber+1)
