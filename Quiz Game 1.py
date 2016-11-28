71a71bbe3092966184c38440b7774f164cf12678
200262 <200262@hkis.edu.hk>
2016-11-28 21:40:53 +0800

    adding comments to explain code

--- Quiz Game 1.py
+++ Quiz Game 1.py
@@ -1,11 +1,9 @@
 name = input("What is your name?")
 name = name.upper()
+#takes name and makes it uppercase
 print ("Hello", name, ", this is my quiz game. It is very simple. ""With every question there will be four possible answers, A through D. Just type "
                       "the letter of your answer. At the end you’ll get your score, along with the correct answers. Press enter to begin. Good luck!")
-
-questionnumber = 0
-
-
+#opening sequence
 questions = ["Question One: What is the seventh planet from the sun? A. Pluto B. Jupiter C. There are only six planets D. Uranus",
              "Question Two: Where would you find the Sea of Tranquility? A. The Moon B. California C. Brazil D. Spain",
              "Question Three: Which chess piece can move diagonally? A. King B. Princess C. Pawn D. Bishop",
@@ -22,38 +20,58 @@ questions = ["Question One: What is the seventh planet from the sun? A. Pluto B.
              "Question Fourteen: How many plays did Shakespeare write? A. 37 B. 12 C. He didn't write them D. 56",
              "Question Fifteen: A rooster laid an egg on top of a barn roof, which way did it roll? A. left B. right C. it broke D. none of the above",
              ]
+#lists all questions
 answers = ["D", "A", "D", "C", "B", "C", "A", "C", "A", "D", "B", "B", "A", "A", "D", ]
+#lists all answers
 totalquestions=len(questions)
+#if you were to add or subtract questions, this takes total number of questions
+questionnumber = 0
+#starts question number (needed later) at zero
 answerinput=0
+#sets a variable for answer input
 score = 0
+#sets a variable for the score
 questionsasked=0
-print (totalquestions)
+#sets a variable to keep of the number of questions asked
 
 def question(questionnumber):
     global answerinput
     global questionsasked
+    #sets a few variables as global so that they don't create problems later
     while questionsasked >= 0 and questionsasked < totalquestions:
+        #starts a while loop that will run from the beginning until you are done with all the questions
         print (questions[questionnumber])
         answerinput = input("Which answer?")
         answerinput=answerinput.upper()
+        #asks for an answer, then converts it to uppercase so that it will match the answers list (which is all in uppercase)
         answer(questionnumber)
+        #goes down to the second function
 
 def answer(questionnumber):
     global questionsasked
+    #sets variable global to prevent problems later
     while questionsasked >= 0 and questionsasked < totalquestions:
+        #starts when no questions have been asked, and ends when all the questions have been asked
         global score
         global answerinput
+        #sets variable global to prevent problems later
         if str(answerinput) == str(answers[questionnumber]):
             score=score+1
             print ("You were correct. You're score is:", str(score))
             questionsasked=questionsasked+1
             question(questionnumber+1)
+            #if the answer is the same as the correct answer then you will be credited with a point, it counts as one more question asked
+            #and the top function runs for the next question
         else:
             print ("You were incorrect, the correct answer was:", str(answers[questionnumber]))
             print ("You're score is:", str(score))
             questionsasked=questionsasked+1
             question(questionnumber+1)
+            #if the answer is incorrect you will be told what the correct answer was, it counts as one more question asked
+            #and the top function runs for the next question
 
 question(questionnumber)
+#starts the code, and because of while loops embedded within the functions, the code will move on automatically
 print ("You have completed the game. You're score was:", score)
+#ending sequence
 
--- Quiz Game 2 V2.py
+++ Quiz Game 2 V2.py
@@ -1,60 +1,81 @@
 import datetime
 todaydate=datetime.date.today()
-
+#importing the date
 
 name = input("What is your name?")
 name = name.upper()
+#getting name and making it uppercase
 print ("Hello", name, ", this is my quiz game. It is very simple. ""With every question there will be four possible answers, A through D. Just type "
                       "the letter of your answer. At the end you’ll get your score, along with the correct answers. Press enter to begin. Good luck!")
-
+#opening sequence
 file = open('questions.txt', "r+")
+#opening a file that contains each question on a separate line
 secondfile = open("Answers.txt", "r+")
-score = 0
+#opening a file that contains each answer on a separate line
 
+score = 0
+#setting a variable for the score
 questionnumber = 0
+#setting a variable for question number
 questionsasked = 0
+#setting a variable for how many questions have been asked
 
 
 
 def askquestion(questionnumber):
     global questionsasked
+    #creating a global variable to prevent problems later
     while questionsasked >= 0 and questionsasked < 15:
+        #running a while loop that will run for the duration of every question
         global score
+        #creating a global variable to prevent problems later
         print(file.readline())
+        #prints the next line in the text file "file", which corresponds to the questions text file
         answer = input("Press the letter that corresponds to the correct answer:")
         answer=str(answer)
         answer=answer.upper()
+        #asks for answer and converts it to uppercase so that it will correspond with the text file which is in uppercase
         correct=secondfile.readline()
         correct=correct[0]
         correct=str(correct)
+        #gets a variable for the correct answer by taking the next line in the text file "secondfile" which corresponds to the answers text file
         if correct==answer:
             score=score+1
             print ("You were correct. Your score is: ", str(score))
             questionsasked=questionsasked+1
             askquestion(questionnumber + 1)
+            #if user is correct, one point is added to their score, the question is accounted for and the loop is run again for the next line
         else:
             print ("You were incorrect. The correct answer is:", correct)
             print ("Your score is: ", str(score))
             questionsasked=questionsasked+1
-            askquestion(questionnumber + 1)
+            askquestion(questionnumber+1)
+            #if user is incorrect, the question is accounted for and the loop is run again for the next line
 
 askquestion(questionnumber)
+#starts running the loop
 
 print (name, ",", str(score), "out of 15 was your score, completed on", todaydate)
+#prints a statement recording your score
 fourthfile=open("List of Scores", "r+")
 fourthfile.writelines(["\n", str(score)])
 fourthfile=sorted(fourthfile.readlines())
 scoreslist=[]
+#opens a file that can be read and written in, adds the user's score to the list of scores and then sorts this list
 for line in fourthfile:
     scoreslist.append(line)
 highscore=int(scoreslist[-1])
-if score > highscore:
+#creates a variable highscore based on the highest score in the text file
+if score >= highscore:
     print ("You have gotten the highest score!")
+    #if their score is the highest, then it tells them
 else:
     print ("The highest score is:", str(highscore), "Keep trying and you may get it.")
+    #if their score is not the highest, it tells them what the highest score is
 thirdfile=open("List of Entries", "a")
+#opens a file that can be appended
 resultline=(name, "got", score, "on", todaydate)
+#creates a result line that can be saved
 resultline=str(resultline)
-print (resultline)
 thirdfile.writelines(resultline)
-
+#adds this result line to the bank of result lines
